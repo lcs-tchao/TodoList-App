08c4d9662a512150e35cb0b975c452ffc0673a51
Thomas Chao <Tchao@lcs.on.ca>
2025-02-01 11:04:53 -0500

    Initial Commit

---
+++ TodoList App/TodoList_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_AppApp.swift
+//  TodoList App
+//
+//  Created by Yuk Yeung Chao on 2025-02-01.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
