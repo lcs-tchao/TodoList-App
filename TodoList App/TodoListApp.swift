550dc96e8f0c38cd44c7ec72c4d8f6ad6c256e97
Thomas Chao <Tchao@lcs.on.ca>
2025-02-01 19:12:27 -0500

    added model container to the app entry point.

--- TodoList App/TodoListApp.swift
+++ TodoList App/TodoListApp.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Yuk Yeung Chao on 2025-02-01.
 //
-
+import SwiftData
 import SwiftUI
 
 @main
@@ -12,6 +12,7 @@ struct TodoListApp: App {
     var body: some Scene {
         WindowGroup {
             LandingView()
+                .modelContainer(for: TodoItem.self)
         }
     }
 }
